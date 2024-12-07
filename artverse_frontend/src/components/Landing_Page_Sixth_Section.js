feea2b3f7b4789e1651b088042f58621312c8eb1
wajeeha kashaf <wajeehakashaf1122@gmail.com>
2024-12-07 07:27:38 +0500

    css_landing_page

--- artverse_frontend/src/components/Landing_Page_Sixth_Section.js
+++ artverse_frontend/src/components/Landing_Page_Sixth_Section.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import '../styles/Landing_Page_Sixth_Section.css';
-import SideImage from '../assets/images/Landing_Page_Image3.png'; // Correct path to your profile icon
-import ArtistsSign from '../assets/images/Artist_Sign.png'; // Correct path to your profile icon
-
-
+import React from "react";
+import "../styles/Landing_Page_Sixth_Section.css?v=1";
+import SideImage from "../assets/images/Landing_Page_Image3.png"; // Correct path to your profile icon
+import ArtistsSign from "../assets/images/Artist_Sign.png"; // Correct path to your profile icon
 
 function Landing_Page_Sixth_Section() {
   return (
-    <div>
-
-      <button className='sixth-button'><b>Explore More</b></button>
-
-      <div className="container1">
-        <div className="image-container">
-          <img
-            src={SideImage}
-            alt="Description"
-            className="styled-image"
-          />
+    <div className="sixth-sec-main">
+      <button className="sixth-sec-button">
+        <b>Explore More</b>
+      </button>
+      <div className="sixth-sec-container">
+        <div className="sixth-sec-image-container">
+          <img src={SideImage} alt="Description" className="sixth-sec-styled-image" />
         </div>
-        <div className="text-container">
-          <h3 className='con-heading'>Find Art You Love</h3>
-          <p className='con-para'>“At Artverse, we make it our mission to help you discover and buy from the best emerging artists around the world. Whether you’re looking to discover a new artist, add a statement piece to your home, or commemorate an important life event, Artverse is your portal to thousands of original works by today’s top artists.”</p>
-
-          <img src={ArtistsSign} alt='' className='con-pic'></img>
-          <h3 className='con-head3'>Chief Curator & VP, Art Advisory</h3>
+        <div className="sixth-sec-text-container">
+          <h3 className="sixth-sec-heading">Find Art You Love</h3>
+          <p className="sixth-sec-para">
+            “At Artverse, we make it our mission to help you discover and buy
+            from the best emerging artists around the world. Whether you’re
+            looking to discover a new artist, add a statement piece to your
+            home, or commemorate an important life event, Artverse is your
+            portal to thousands of original works by today’s top artists.”
+          </p>
+          <img src={ArtistsSign} alt="Artist Signature" className="sixth-sec-pic" />
+          <h3 className="sixth-sec-head3">Chief Curator & VP, Art Advisory</h3>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default Landing_Page_Sixth_Section;
