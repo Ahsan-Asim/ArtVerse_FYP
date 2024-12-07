d64dfadd28a9242f87529bc7ce32c5c35bf4421e
wajeeha kashaf <wajeehakashaf1122@gmail.com>
2024-12-07 08:45:21 +0500

    cartpage css

--- artverse_frontend/src/pages/CartPage.js
+++ artverse_frontend/src/pages/CartPage.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Item_cart from "../components/Item_cart.js";
 import "../styles/Cart_page.css";
+import ArtistStudioHeader from "../components/Artist_studio_header.js";
+import Item_cart from "../components/Item_cart.js";
+import ArtistStudioFooter from "../components/Artist_studio_footer.js";
 
 function CartPage() {
   return (
     <div className="cartpage">
+      <ArtistStudioHeader />
       <Item_cart />
+      <ArtistStudioFooter />
     </div>
   );
 }
